87afe2e47ee347e343f54088be713c548995cbaa
prajwal <prajwalmelath@gmail.com>
2020-06-22 13:02:57 +0530

    added navigation

--- app/src/main/java/com/shalaka/facilitymonitoringsystem/Activity/MainActivity.java
+++ app/src/main/java/com/shalaka/facilitymonitoringsystem/Activity/MainActivity.java
@@ -1,10 +1,17 @@
 package com.shalaka.facilitymonitoringsystem.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import com.shalaka.facilitymonitoringsystem.R;
 import com.shalaka.facilitymonitoringsystem.fragment.Mainscreen;
@@ -14,10 +21,22 @@ public class MainActivity extends AppCompatActivity {
     FragmentTransaction fragmentTransaction;
     Mainscreen assetListFragment;
 
+    /*private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       /*
+       mDrawerLayout= (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
 
         assetListFragment = new Mainscreen();
         fragmentManager = getSupportFragmentManager();
@@ -26,4 +45,15 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
     }
+
+   /* @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }*/
+
 }
